a5f4d28b59f749380c1522f7ebd3e7cd2c251a13
Tung Nguyen <nh.tung@outlook.com>
2024-07-11 02:09:42 +0700

    check code 13

--- apps/rsa_api.py
+++ apps/rsa_api.py
@@ -416,7 +416,7 @@ class RsaController(ControllerBase):
             print("message", message, "encrypted message", encrypted_message)
             message_send = {
                 'hostname': hostname,
-                'encrypted message': encrypted_message
+                'encrypted_message': encrypted_message
             }
             end_point = '/secure_message'
             url = 'http://{0}:8080{1}'.format(hostname_peer, end_point)
