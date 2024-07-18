7adf64b833cf42c472e0e65ab256f05dbc44b534
Tung Nguyen <nh.tung@outlook.com>
2024-07-18 22:35:28 +0700

    complete rsa p2p flow and topology sync 10

--- apps/tung_flow_topo_onReceive_sync.py
+++ apps/tung_flow_topo_onReceive_sync.py
@@ -197,7 +197,7 @@ class TopologyController(ControllerBase):
 
         elif action == 'topology-update':
             data = decrypted_message['topology']
-            topo = json.loads(topo)
+            topo = json.loads(data)
             send_secure_topology(topo, peers_to_exclude, peers)
             client = MongoClient('mongodb://localhost:27017/')
             db = client['sdn']  
