f40ac775d3ccff78289de49ececb85dcb534f0c6
Tung Nguyen <nh.tung@outlook.com>
2024-07-20 10:19:48 +0700

    add web page 8

--- web_page.py
+++ web_page.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 import requests
 import subprocess
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
