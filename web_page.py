64bc22af630854286ee2183dc0d77bec4047540b
Tung Nguyen <nh.tung@outlook.com>
2024-07-20 09:31:59 +0700

    add web page 3

--- web_page.py
+++ web_page.py
@@ -2,12 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/p2p/flow_mod')
+@app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/p2p/flow_mod')
+def flow_modify():
     return render_template('flow_modify.html')
 
 @app.route('/p2p/connect')
-def index():
+def peer_connect():
     return render_template('peer_connect.html')
 
 
