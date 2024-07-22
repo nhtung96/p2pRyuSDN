9d4e5c0fbf974f9c351c9768fa8756590a368cfd
Tung Nguyen <nh.tung@outlook.com>
2024-07-22 23:57:14 +0700

    topology global 11

--- web_page.py
+++ web_page.py
@@ -126,30 +126,6 @@ def proxy_topology_local(hostname):
     except requests.RequestException as e:
         return str(e), 500
     
-
-@app.route('/p2p/topology/gui', methods=['GET'])
-def topology_gui():
-    return render_template('topology_local_load.html')
-
-@app.route('/p2p/topology/gui/<hostname>', methods=['GET'])
-def proxy_topology_gui(hostname):
-    try:
-        # Construct the URL to fetch the webpage from the specified hostname
-        url = f'http://{hostname}:8080/p2p/topology'
-        
-        # Forward the request to the target server
-        response = requests.get(url, headers=request.headers, stream=True)
-        response.raise_for_status()  # Check for HTTP errors
-        
-        # Create a new response with the content from the target server
-        return Response(
-            response.iter_content(chunk_size=1024),
-            content_type=response.headers.get('Content-Type'),
-            status=response.status_code
-        )
-    except requests.RequestException as e:
-        return str(e), 500
-
 @app.route('/p2p/topology/gui', methods=['GET', 'POST'])
 def topology_gui():
     if request.method == 'POST':
